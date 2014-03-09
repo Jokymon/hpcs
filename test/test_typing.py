6c1b7cc5d0771cda815ad8f9013146b6a72f10d9
Silvan Wegmann <swegmann@narf.ch>
2014-03-09 21:22:49 +0100

    Increase test depth for the typing component

--- test/test_typing.py
+++ test/test_typing.py
@@ -11,6 +11,12 @@ class TestTypeEquality:
         assert v1 == v2
         assert v1 == v1
 
+    def testVoidInequality(self):
+        v = Void()
+
+        assert v != Int8
+        assert v != Int16
+
     def testIntegerEquality(self):
         i8 = Int8
         i16 = Int16
@@ -27,6 +33,18 @@ class TestTypeEquality:
         func2 = Function(Void(), [])
 
         assert func1 == func2
+
+    def testSimpleFunctionInequality(self):
+        func1 = Function(Void(), [])
+        func2 = Function(Int8, [])
+        func3 = Function(Void(), [Int8, Int8])
+
+        assert func1 != Void()
+        assert func2 != Int8
+
+        assert func1 != func2
+        assert func2 != func3
+        assert func3 != func1
 
 
 @pytest.fixture
@@ -53,6 +71,9 @@ class TestTypeGenerationById:
         assert typesystem.get_type_for_id("Int16") == Int16
         assert typesystem.get_type_for_id("Int32") == Int32
 
+    def testUnknownType(self, typesystem):
+        assert typesystem.get_type_for_id("some_unknown_id") is None
+
 
 class TestTypeResolution:
     def testInt8Adding(self, typesystem):
@@ -60,3 +81,6 @@ class TestTypeResolution:
 
     def testInt8Int16Adding(self, typesystem):
         assert typesystem.resolve_types(Int8, Int16, ast.Add) == Int16
+
+    def testInt16Int8Adding(self, typesystem):
+        assert typesystem.resolve_types(Int16, Int8, ast.Add) == Int16
