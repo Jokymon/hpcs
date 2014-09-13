a96c97a33bc3905d3a09d453d742445d28d70116
Silvan Wegmann <swegmann@narf.ch>
2014-09-13 20:00:44 +0200

    Update LLVM version to 3.3.0 and llvmpy to 0.12.7

--- scripts/prepare_win.bat
+++ scripts/prepare_win.bat
@@ -1,6 +1,7 @@
 pip install wget
-python -m wget http://repo.continuum.io/pkgs/free/win-64/llvm-3.2-0.tar.bz2
-python scripts\untar.py llvm-3.2-0.tar.bz2
+python -m wget http://repo.continuum.io/pkgs/free/win-64/llvm-3.3-0.tar.bz2
+python scripts\untar.py llvm-3.3-0.tar.bz2
 path=%PATH%;c:\src\hpcs\Library\bin
-python -m wget http://repo.continuum.io/pkgs/free/win-64/llvmpy-0.12.6-py34_0.tar.bz2
-python scripts\untar.py llvmpy-0.12.6-py34_0.tar.bz2
+python -m wget http://repo.continuum.io/pkgs/free/win-64/llvmpy-0.12.7-py34_0.tar.bz2
+python scripts\untar.py llvmpy-0.12.7-py34_0.tar.bz2
+set PYTHONPATH=c:\src\hpcs\Lib\site-packages
